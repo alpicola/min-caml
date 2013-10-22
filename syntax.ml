967672da191cd4babf79150be2773e6b7d27f0b8
alpicola <ryo@alpico.la>
2013-10-22 18:44:06 +0900

    pretty printer

--- syntax.ml
+++ syntax.ml
@@ -25,3 +25,41 @@ type t = (* MinCamlの構文を表現するデータ型 (caml2html: syntax_t) *)
   | Get of t * t
   | Put of t * t * t
 and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
+
+let pp e =
+  let paren s = "(" ^ s ^ ")" in
+  let ppn (x, t) = x ^ ":" ^ Type.pp t in
+  let rec f i e = String.make i ' ' ^ match e with
+    | Unit -> "Unit"
+    | Bool b -> "Bool" ^ paren (string_of_bool b)
+    | Int i -> "Int" ^ paren (string_of_int i)
+    | Float f -> "Float" ^ paren (string_of_float f)
+    | Not e -> "Not\n" ^ f (i + 2) e
+    | Neg e -> "Neg\n" ^ f (i + 2) e
+    | Add (e1, e2) -> "Add\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Sub (e1, e2) -> "Sub\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | FNeg e -> "FNeg\n" ^ f (i + 2) e
+    | FAdd (e1, e2) -> "FAdd\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | FSub (e1, e2) -> "FSub\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | FMul (e1, e2) -> "FMul\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | FDiv (e1, e2) -> "FDiv\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Eq (e1, e2) -> "Eq\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | LE (e1, e2) -> "LE\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | If (e1, e2, e3) ->
+        "If\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2 ^ "\n" ^ f (i + 2) e3
+    | Let (n, e1, e2) ->
+        "Let" ^ paren (ppn n) ^ "\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Var x -> "Var" ^ paren x
+    | LetRec({ name = n; args = ns; body = e1 }, e2) ->
+        "LetRec" ^ paren (String.concat ", " (List.map ppn (n :: ns))) ^
+        "\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | App (e, es) -> "App\n" ^ String.concat "\n" (List.map (f (i + 2)) (e :: es))
+    | Tuple es -> "Tuple\n" ^ String.concat "\n" (List.map (f (i + 2)) es)
+    | LetTuple (ns, e1, e2) ->
+        "LetTuple" ^ paren (String.concat ", " (List.map ppn ns)) ^
+        "\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Array (e1, e2) -> "Array\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Get (e1, e2) -> "Get\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2
+    | Put (e1, e2, e3) ->
+        "Put\n" ^ f (i + 2) e1 ^ "\n" ^ f (i + 2) e2 ^ "\n" ^ f (i + 2) e3
+  in f 0 e
--- type.ml
+++ type.ml
@@ -9,3 +9,13 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec pp = function
+  | Unit -> "unit"
+  | Bool -> "bool"
+  | Int -> "int"
+  | Float -> "float"
+  | Fun (ts, t) -> "(" ^ String.concat ", " (List.map pp ts) ^ ") -> " ^ pp t
+  | Tuple ts -> "(" ^ String.concat " * " (List.map pp ts) ^ ")"
+  | Array t -> pp t ^ " array"
+  | Var r -> match !r with Some t -> pp t | None -> "none"
